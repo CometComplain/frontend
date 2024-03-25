ea26634cbb30872ef9abd0dca6200e5a554e6122
sasaank-pankam <sasaankme@gmail.com>
2024-03-25 22:38:33 +0530

    removed google oauth content

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,9 @@ import App from './App.jsx'
 import './index.css'
 import {GoogleOAuthProvider} from "@react-oauth/google";
 
-const clientId = '917898502057-8docqh7qo20sftib30o3bgndpvs4uaoa.apps.googleusercontent.com';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <GoogleOAuthProvider clientId={clientId}>
         <React.StrictMode>
             <App/>
         </React.StrictMode>
-    </GoogleOAuthProvider>
 )
